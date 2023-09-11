dc5e945bec952810dd17627584ca18a07ad5ad81
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-11 18:51:48 +0200

    add: HomeScreen

---
+++ screens/HomeScreen/HomeScreen.styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../constants/Colors';
+
+export default StyleSheet.create({
+    container: {
+      backgroundColor: Colors.background,
+      flex: 1
+    }
+})
