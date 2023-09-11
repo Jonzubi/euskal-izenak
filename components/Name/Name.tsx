4ea614ffa82c13c48e91ef3c5b12095d09524768
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-11 22:58:50 +0200

    style: Ampliamos icono del genero

--- components/Name/Name.tsx
+++ components/Name/Name.tsx
@@ -12,7 +12,7 @@ const Name = ({ objName: { name, gender, origin} }: NameProps) => {
   return (
     <View style={[styles.container, gender === 'Mutila' && styles.backgroundBoy, gender === 'Neska' && styles.backgroundGirl]}>
       <Text style={styles.name}>{name}</Text>
-      <Ionicons name={gender === 'Mutila' ? 'male' : 'female'} size={100} color="white" style={{ alignSelf: 'center'}} />
+      <Ionicons name={gender === 'Mutila' ? 'male' : 'female'} size={125} color="white" style={{ alignSelf: 'center'}} />
       <View style={[styles.originContainer, gender === 'Mutila' && styles.originContainerBoy, gender === 'Neska' && styles.originContainerGirl]}>
         <Text style={styles.originTitle}>Jatorria:</Text>
         <Text style={styles.origin}>{origin}</Text>
